7614adbb7f64559372f05384906271791a94e9f8
Mattias Lambert <mattiasl380@gmail.com>
2019-04-11 18:16:22 -0400

    stuff

--- Loom/Assets/Scripts/ParticleBells.cs
+++ Loom/Assets/Scripts/ParticleBells.cs
@@ -22,6 +22,10 @@ public class ParticleBells : MonoBehaviour
     public float distanceThreshold;
 
     public AK.Wwise.Event PlayBellEvent;
+
+    private bool playing;
+
+    private GameObject soundObject;
     
     // Start is called before the first frame update
     void Start()
@@ -33,40 +37,35 @@ public class ParticleBells : MonoBehaviour
         ParticleSystem.Particle chosenParticle = allParts[Random.Range(0, allParts.Length)];
         particlePosition = new Vector2(chosenParticle.position.x, chosenParticle.position.z);
         Debug.Log("New Particle Position: " + particlePosition);
+        soundObject = new GameObject("BellSoundObject");
+        soundObject.transform.parent = transform;
+        soundObject.transform.localPosition = particlePosition;
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
-        if (t >= switchFrequency)
+        if (!playing)
         {
-            t = 0f;
-            switchFrequency = Random.Range(switchFrequencyMin, switchFrequencyMax);
-            myPartSystem.GetParticles(allParts);
-            ParticleSystem.Particle chosenParticle = allParts[Random.Range(0, allParts.Length)];
-            particlePosition = new Vector2(chosenParticle.position.x, chosenParticle.position.z);
-            Debug.Log("New Particle Position: " + particlePosition);
-            GameObject go = new GameObject();
-            go.transform.position = particlePosition;
-            PlayBellEvent.Post(go);
+            StartCoroutine(PlaySound());
         }
 
-        // Vector2 playerPos = new Vector2(player.position.x, player.position.z);
-        // float dist = Vector2.Distance(playerPos, particlePosition);
+    }
 
-        
-        /*
-        if (dist < distanceThreshold)
-        {
-            // play sound
-            PlayBellEvent.Post(gameObject);
-            Debug.Log("Playing Bell Sound!");
-            t = switchFrequency + 1f;
-        }
-        */
-
+    IEnumerator PlaySound()
+    {
+        playing = true;
+        switchFrequency = Random.Range(switchFrequencyMin, switchFrequencyMax);
+        myPartSystem.GetParticles(allParts);
+        ParticleSystem.Particle chosenParticle = allParts[Random.Range(0, allParts.Length)];
+        particlePosition = new Vector2(chosenParticle.position.x, chosenParticle.position.z);
+        Debug.Log("New Particle Position: " + particlePosition);
+        soundObject.transform.localPosition = particlePosition;
+        PlayBellEvent.Post(soundObject);
+        yield return new WaitForSeconds(switchFrequency);
+        playing = false;
     }
     
     
