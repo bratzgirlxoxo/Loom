6cf18d4fc0da400e50a61110af31b1c340d6a3be
Mattias Lambert <mattiasl380@gmail.com>
2019-04-11 16:09:42 -0400

    no message

--- Loom/Assets/Scripts/ParticleBells.cs
+++ Loom/Assets/Scripts/ParticleBells.cs
@@ -6,7 +6,10 @@ public class ParticleBells : MonoBehaviour
 {
     private ParticleSystem.Particle[] allParts;
 
-    public float switchFrequency;
+    public float switchFrequencyMin;
+    public float switchFrequencyMax;
+
+    private float switchFrequency;
 
     private ParticleSystem myPartSystem;
 
@@ -23,7 +26,7 @@ public class ParticleBells : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        switchFrequency = Random.Range(switchFrequencyMin, switchFrequencyMax);
         myPartSystem = GetComponent<ParticleSystem>();
         allParts = new ParticleSystem.Particle[myPartSystem.main.maxParticles];
         myPartSystem.GetParticles(allParts);
@@ -40,17 +43,21 @@ public class ParticleBells : MonoBehaviour
         if (t >= switchFrequency)
         {
             t = 0f;
-            
+            switchFrequency = Random.Range(switchFrequencyMin, switchFrequencyMax);
             myPartSystem.GetParticles(allParts);
             ParticleSystem.Particle chosenParticle = allParts[Random.Range(0, allParts.Length)];
             particlePosition = new Vector2(chosenParticle.position.x, chosenParticle.position.z);
             Debug.Log("New Particle Position: " + particlePosition);
+            GameObject go = new GameObject();
+            go.transform.position = particlePosition;
+            PlayBellEvent.Post(go);
         }
 
-        Vector2 playerPos = new Vector2(player.position.x, player.position.z);
+        // Vector2 playerPos = new Vector2(player.position.x, player.position.z);
+        // float dist = Vector2.Distance(playerPos, particlePosition);
 
-        float dist = Vector2.Distance(playerPos, particlePosition);
-
+        
+        /*
         if (dist < distanceThreshold)
         {
             // play sound
@@ -58,6 +65,7 @@ public class ParticleBells : MonoBehaviour
             Debug.Log("Playing Bell Sound!");
             t = switchFrequency + 1f;
         }
+        */
 
     }
     
