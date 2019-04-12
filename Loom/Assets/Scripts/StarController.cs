4f16b64158d4957396fbbdb5ffc478b080c8a371
Mattias Lambert <mattiasl380@gmail.com>
2019-04-12 00:51:37 -0400

    Messed with the stars, started working on a dev tool

--- Loom/Assets/Scripts/StarController.cs
+++ Loom/Assets/Scripts/StarController.cs
@@ -7,9 +7,52 @@ public class StarController : MonoBehaviour
 
     public Transform player;
 
-    // Update is called once per frame
+    private float t;
+    
+    // Particle Stuff
+    private ParticleSystem myPartSystem;
+    private ParticleSystem.Particle[] myParts;
+    //private int[] specialIndices;
+
+    public float flickerSpeed;
+
+
+    void Start()
+    {
+        myPartSystem = GetComponent<ParticleSystem>();
+        myParts = new ParticleSystem.Particle[myPartSystem.main.maxParticles];
+        //specialIndices = new int[myParts.Length / 10];
+    }
+    
     void Update()
     {
         transform.position = new Vector3(player.position.x, 0f, player.position.z);
+
+
+
+
+        if (t >= 0f)
+            t += Time.deltaTime;
+        else
+        {
+            myPartSystem.GetParticles(myParts);
+            for (int i = 0; i < myParts.Length; i += 10)
+            {
+                myParts[i].startSize = Mathf.PerlinNoise(myParts[i].position.x, Time.time * flickerSpeed)*2.5f;
+            }
+            myPartSystem.SetParticles(myParts);
+        }
+        
+        // this is only gonna happen once
+        if (t >= 7.5f)
+        {
+            t = -1;
+            myPartSystem.GetParticles(myParts);
+        }
+        
+        
+        
+        
+        
     }
 }
