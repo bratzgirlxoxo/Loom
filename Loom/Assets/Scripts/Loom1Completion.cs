2ea51c8142eabd2fa094dd4851817b5e6c307757
Mattias Lambert <mattiasl380@gmail.com>
2019-04-25 15:29:55 -0400

    a few more fixes

--- Loom/Assets/Scripts/Loom1Completion.cs
+++ Loom/Assets/Scripts/Loom1Completion.cs
@@ -7,68 +7,22 @@ public class Loom1Completion : MonoBehaviour
 {
     public bool ready;
 
-    public FirstPersonController playerController;
-
-    public Transform[] cutPositions;
-    public float[] cutWaitTimes;
 
     public TutorialGhost ghostFriend;
-    
-    public GameObject playerCam;
-    private Camera thisCam;
 
     public AK.Wwise.Event fireballPlay;
 
-    void Start()
-    {
-        thisCam = GetComponent<Camera>();
-        thisCam.enabled = false;
-    }
+    
     
     void Update()
     {
         if (ready)
         {
-            StartCoroutine(CameraCutscene());
+            fireballPlay.Post(ghostFriend.gameObject);
+            ghostFriend.GetComponent<ParticleSystem>().Play();
+            ghostFriend.StartCoroutine(ghostFriend.JumpTutorial());
             ready = false;
         }
-    }
-
-    IEnumerator CameraCutscene()
-    {
-        yield return new WaitForSeconds(3.75f);
-        //thisCam.enabled = true;
-        //playerController.canWalk = false;
-        //playerCam.SetActive(false);
-
-        fireballPlay.Post(ghostFriend.gameObject);
-        ghostFriend.GetComponent<ParticleSystem>().Play();
-        ghostFriend.StartCoroutine(ghostFriend.JumpTutorial());
-
-        /*
-        for (int i = 0; i < cutPositions.Length; i++)
-        {
-            CameraCut(cutPositions[i]);
-            if (i == cutPositions.Length - 1)
-            {
-                fireballPlay.Post(ghostFriend.gameObject);
-                ghostFriend.GetComponent<ParticleSystem>().Play();
-                ghostFriend.StartCoroutine(ghostFriend.JumpTutorial());
-            }
-            yield return new WaitForSeconds(cutWaitTimes[i]);
-        }
-        */
-        
-
-        //playerCam.SetActive(true);
-        //playerController.canWalk = true;
-    }
-
-    // for directly cutting from one camera position to another
-    void CameraCut(Transform newPosition)
-    {
-        thisCam.transform.position = newPosition.position;
-        thisCam.transform.forward = newPosition.forward;
     }
 
 }
--- Loom/Assets/Scripts/LoomTrigger.cs
+++ Loom/Assets/Scripts/LoomTrigger.cs
@@ -56,13 +56,13 @@ public class LoomTrigger : MonoBehaviour
             }
         }
 
-        /*        
+             
         if (fullyLit && cutSceneReady)
         {
             cutScene.ready = true;
             cutSceneReady = false;
         }
-        */
+        
         
 
         if (emerging)
